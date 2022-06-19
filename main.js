7503987864dff50a54f8dba5cb22fb3338d7d060
Mauro Reis <mauroacsreis@gmail.com>
2022-06-19 17:00:29 -0300

    Update main.js
    Added second solution

--- main.js
+++ main.js
@@ -15,3 +15,28 @@ for(let i = 1; i <= 100; i++) {
 }
 }
     fizzBuzz();
+
+// OR
+var output = [];
+var count = 1;
+function fizzBuzz() {
+while(count <= 100) {
+if (count % 3 === 0 && count % 5 === 0) {
+output.push("FizzBuzz");
+}
+else if (count % 3 === 0) {
+output.push("Fizz");
+}
+else if (count % 5 === 0) {
+output.push("Buzz");
+}
+else {
+output.push(count);
+}
+count++;
+}
+console.log(output);
+}
+fizzBuzz();
+}
+console.log(output);
