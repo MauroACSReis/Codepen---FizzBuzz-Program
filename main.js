12c4a5d6335807e8bef02ac516919a3d832585e1
Mauro Reis <mauroacsreis@gmail.com>
2022-06-19 17:48:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -39,3 +39,19 @@ console.log(output);
 fizzBuzz();
 }
 console.log(output);
+
+// OR
+var output = [];
+function fizzBuzz() {
+for(var count = 1; count < 101; count++) {
+if (count % 3 === 0 && count % 5 === 0) {
+output.push("FizzBuzz");
+} else if (count % 3 === 0) {
+} else if (count % 5 === 0) {
+output.push("Buzz");
+} else {
+output.push(count);
+}
+}
+console.log(output);
+}
