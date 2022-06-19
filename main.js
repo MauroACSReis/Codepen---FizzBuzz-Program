ba90b4da587e13cc8f23fe4c08cb370c3e5c3cd7
Mauro Reis <mauroacsreis@gmail.com>
2022-06-19 12:40:05 -0300

    Create main.js
    Created my initial answer but the numbers are printed along with the string and not just the string. Will come back to think about this one.

---
+++ main.js
@@ -0,0 +1,17 @@
+// My initial answer:
+
+function fizzBuzz() {
+for(let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (
+    (i%3 === 0) && (i%5 === 0)) {
+        console.log("FizzBuzz");
+    }else if (i%3 === 0) {
+        console.log("Fizz");
+    } else if (
+       (i%5 === 0)) {
+        console.log("Buzz");
+    }
+}
+}
+    fizzBuzz();
